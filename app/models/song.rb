3c0b0bea1cecd8aac6bf1df7d427bb018df2083b
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-12-09 11:22:05 -0500

    commit tree 6757c21bc3a34b9d3518b889836c62d97603c1c5

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,9 +2,6 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
-  def get_genre_name
-  end
-
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
